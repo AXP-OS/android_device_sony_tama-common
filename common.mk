54f2d1eede1a9378481f7e15548d60e3561f553c
steadfasterX <steadfasterX@users.noreply.github.com>
2024-07-17 14:58:27 +0200

    mk: enable AXP.OS

--- common.mk
+++ common.mk
@@ -453,3 +453,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096
+
+# enable AXP.OS vendor
+$(call inherit-product, vendor/axp/config/common.mk)
