c3e60954f78a7a6a3422ee4087a238220a028751
steadfasterX <steadfasterX@users.noreply.github.com>
2025-04-22 11:04:12 +0200

    mk: enable AXP.OS boardconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,3 +187,7 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 -include vendor/sony/tama-common/BoardConfigVendor.mk
+
+# even though we include vendor/axp/config/common.mk we need to include AXP's own BoardConfig
+# (after the above definitions & includes), too so we we can make use of the conditions within
+include vendor/axp/BoardConfigVendor.mk
